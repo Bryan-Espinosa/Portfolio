59e776a415a1e61e6a93373cddff2c91ed757bc8
bryan espinosa <bmanxy@gmail.com>
2018-09-12 20:19:25 -0500

    broken css

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,8 @@
 import React from "react";
 import "./css/Contact.css";
+import "../photos/gmail.png";
+import "../photos/githubIcon.pmg.png";
+import "../photos/linkedin.png";
 
 export default function Contact() {
   return (
@@ -7,36 +10,39 @@ export default function Contact() {
       <div className="box-info">
         <p className="info">Phone : (682)-552-1412</p>
         <p className="info">
-          Email :{" "}
           <a
             className="info"
-            href="mailto:
-          
-          bryan.espinosa117@gmail.com"
+            href="mailto: bryan.espinosa117@gmail.com"
             target="_blank"
           >
-            {" "}
+            <img
+              className="links"
+              src={require("../photos/gmail.png")}
+              alt=" gmail"
+            />
             bryan.espinosa117@gmail.com
           </a>
         </p>
         <p className="info">
-          {" "}
-          LinkedIn :{" "}
           <a
             className="info"
             href="https://linkedin.com/in/bryan-s-espinosa/"
             target="_blank"
           >
+            <img src={require("../photos/linkedin.png")} alt=" LinkedIn : " />
             linkedin.com/in/bryan-s-espinosa/
           </a>
         </p>
         <p className="info">
-          Github :
           <a
             className="info"
             href="https://github.com/Bryan-Espinosa/"
             target="_blank"
           >
+            <img
+              src={require("../photos/githubIcon.pmg.png")}
+              alt=" Github : "
+            />
             github.com/Bryan-Espinosa/
           </a>
         </p>
